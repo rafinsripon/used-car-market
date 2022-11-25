120cca2190194c7fefc798160eba0f4072dc1685
rafinsripon <rafinsripon@gmail.com>
2022-11-26 00:16:34 +0600

    added blog section and some quetions add

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,146 +1,152 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 mt-16 pb-12">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <p className="inline-block px-6 py-px mb-4 text-base font-semibold tracking-wider text-white uppercase rounded-full bg-sky-600">
-              Our Blog
-            </p>
-          </div>
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="currentColor"
-                className="absolute top-0 left-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block -z-50"
-              >
-                <defs>
-                  <pattern
-                    id="ea469ae8-e6ec-4aca-8875-fc402da4d16e"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#ea469ae8-e6ec-4aca-8875-fc402da4d16e)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
-              <span className="relative -z-50">Some</span>
-            </span>{" "}
-            Most important interviews question
+  return (
+    <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 mt-16 pb-12">
+      
+
+      <div
+        tabIndex={0}
+        className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
+      >
+        <div className="collapse-title text-2xl font-bold text-slate-900">
+          What are the different ways to manage a state in a React application?
+        </div>
+        <div className="collapse-content">
+          <h2 className="text-secondary font-bold text-lg">
+            There are four main types of state you need to properly manage in
+            your React apps:
           </h2>
-          <p className="text-base text-gray-700 md:text-lg">
-            An effective FAQ resource can educate, inform, and naturally guide the
-            user through your website’s content and toward the goals.
+          <li className="list-decimal font-bold text-gray-600">Local state</li>
+          <li className="list-decimal font-bold text-gray-600">Global State</li>
+          <li className="list-decimal font-bold text-gray-600">Server State</li>
+          <li className="list-decimal font-bold text-gray-600">URL state</li>
+          <span className="font-bold text-base">Local (UI) state :– </span>
+          <p className="font-bold text-gray-600 text-base">
+            Local state is data we manage in one or another component.For
+            example, local state would be needed to show or hide a modal
+            component or to track values for a form component, such as form
+            submission, when the form is disabled and the values of a form’s
+            inputs.
+          </p>
+
+          <span className="font-bold text-base">Global State :– </span>
+          <p className="font-bold text-gray-600">
+            Global state is data we manage across multiple components.A common
+            example of global state is authenticated user state. If a user is
+            logged into our app, it is necessary to get and change their data
+            throughout our application.
+          </p>
+          <span className="font-bold text-base">Server State:– </span>
+          <p className="font-bold text-gray-600">
+            Data that comes from an external server that must be integrated with
+            our UI state.There are several pieces of state that must be managed
+            every time you fetch or update data from an external server,
+            including loading and error state.
+          </p>
+          <span className="font-bold text-base">URL state:– </span>
+          <p className="font-bold text-gray-600">
+            Data that exists on our URLs, including the pathname and query
+            parameters.URL state is often missing as a category of state, but it
+            is an important one. In many cases, a lot of major parts of our
+            application rely upon accessing URL state.
           </p>
         </div>
-  
-        <div
-          tabIndex={0}
-          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
-        >
-          <div className="collapse-title text-2xl font-bold text-slate-900">
-            Difference between SQL and NoSQL??
-          </div>
-          <div className="collapse-content">
-            <p className="font-bold text-gray-600">
-              SQL is the programming language used to interface with relational
-              databases. (Relational databases model data as records in rows and
-              tables with logical links between them). NoSQL is a class of DBMs
-              that are non-relational and generally do not use SQL.
-            </p>
-            <h2 className="text-xl font-bold text-slate-900 mt-3">
-              There are five practical differences between SQL and NoSQL:
-            </h2>
-            <li className="list-decimal font-bold text-gray-600">Language</li>
-            <li className="list-decimal font-bold text-gray-600">Scalability</li>
-            <li className="list-decimal font-bold text-gray-600">Structure</li>
-            <li className="list-decimal font-bold text-gray-600">Properties</li>
-            <li className="list-decimal font-bold text-gray-600">
-              Support and communities
-            </li>
-          </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
+      >
+        <div className="collapse-title text-2xl font-bold text-slate-900">
+          How does prototypical inheritance work?
         </div>
-        <div
-          tabIndex={0}
-          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
-        >
-          <div className="collapse-title text-2xl font-bold text-slate-900">
-            What is JWT, and how does it work?
-          </div>
-          <div className="collapse-content">
-            <p className="font-bold text-gray-600">
-              For beginning developers, JSON stands for JavaScript Object Notation
-              and is a text-based format for transmitting data across web
-              applications. It stores information in an easy-to-access manner,
-              both for developers and computers. It can be used as a data format
-              by any programming language and is quickly becoming the preferred
-              syntax for APIs, surpassing XML. JWTs differ from other web tokens
-              in that they contain a set of claims. Claims are used to transmit
-              information between two parties. What these claims are depends on
-              the use case at hand. For example, a claim may assert who issued the
-              token, how long it is valid for, or what permissions the client has
-              been granted.
-            </p>
-          </div>
-        </div>
-        <div
-          tabIndex={0}
-          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
-        >
-          <div className="collapse-title text-2xl font-bold text-slate-900">
-            What is the difference between javascript and NodeJS?
-          </div>
-          <div className="collapse-content">
-            <li className="list-none font-bold text-gray-600">
-              <span className="text-xl font-bold text-gray-800">1. NodeJS :</span>{" "}
-              NodeJS is a cross-platform and opensource Javascript runtime
-              environment that allows the javascript to be run on the server-side.
-              Nodejs allows Javascript code to run outside the browser. Nodejs
-              comes with a lot of modules and mostly used in web development.
-            </li>
-            <li className="list-none font-bold text-gray-600">
-              <span className="text-xl font-bold text-gray-800">
-                2. JavaScript :
-              </span>{" "}
-              Javascript is a Scripting language. It is mostly abbreviated as JS.
-              It can be said that Javascript is the updated version of the ECMA
-              script. Javascript is a high-level programming language that uses
-              the concept of Oops but it is based on prototype inheritance.
-            </li>
-          </div>
-        </div>
-        <div
-          tabIndex={0}
-          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
-        >
-          <div className="collapse-title text-2xl font-bold text-slate-900">
-            How does NodeJS handle multiple requests at the same time?
-          </div>
-          <div className="collapse-content">
-            <p className="font-bold text-gray-600">
-              NodeJS receives multiple client requests and places them into
-              EventQueue. NodeJS is built with the concept of event-driven
-              architecture. NodeJS has its own EventLoop which is an infinite loop
-              that receives requests and processes them. EventLoop is the listener
-              for the EventQueue. If NodeJS can process the request without I/O
-              blocking then the event loop would itself process the request and
-              sends the response back to the client by itself. But, it is possible
-              to process multiple requests parallelly using the NodeJS cluster
-              module or worker_threads module.
-            </p>
-          </div>
+        <div className="collapse-content">
+          <p className="font-bold text-gray-600">
+            The Prototypal Inheritance is a feature in javascript used to add
+            methods and properties in objects. It is a method by which an object
+            can inherit the properties and methods of another object.
+            Traditionally, in order to get and set the (Prototype) of an object,
+            we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in
+            modern language, it is being set using
+          </p>
         </div>
       </div>
-    );
+      <div
+        tabIndex={0}
+        className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
+      >
+        <div className="collapse-title text-2xl font-bold text-slate-900">
+          What is a unit test? Why should we write unit tests?
+        </div>
+        <div className="collapse-content">
+          <li className="list-none font-bold text-gray-600">
+            Unit testing is a software development process in which the smallest
+            testable parts of an application, called units, are individually and
+            independently scrutinized for proper operation. This testing
+            methodology is done during the development process by the software
+            developers and sometimes QA staff. The main objective of unit
+            testing is to isolate written code to test and determine if it works
+            as intended. Unit testing is an important step in the development
+            process, because if done correctly, it can help detect early flaws
+            in code which may be more difficult to find in later testing stages.
+          </li>
+          <li className="list-none font-bold text-gray-600">
+            A unit test typically comprises of three stages: plan, cases and
+            scripting and the unit test itself. In the first step, the unit test
+            is prepared and reviewed. The next step is for the test cases and
+            scripts to be made, then the code is tested. Test-driven development
+            requires that developers first write failing unit tests. Then they
+            write code and refactor the application until the test passes. TDD
+            typically results in an explicit and predictable code base.
+          </li>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
+      >
+        <div className="collapse-title text-2xl font-bold text-slate-900">
+          React vs. Angular vs. Vue?
+        </div>
+        <div className="collapse-content">
+          <p className="text-lg font-bold">React js Vs Angular</p>
+          <p className="font-bold text-gray-600">
+            If the choice you’re making is based on Angular vs React alone, then
+            you’ll simply need to consider the pros and cons discussed for those
+            libraries in this post. But overall, keep in mind that both
+            libraries can be used for mobile and web apps, while Angular is
+            generally better for more complex apps that are enterprise-ready.
+            React often requires extra modules and components, which keeps the
+            core library small, but means there’s extra work involved when
+            incorporating outside tools. Angular, on the other hand, is more of
+            a full-fledged solution that doesn’t require extras like React often
+            does, though it does have a steeper learning curve for its core
+            compared to React. React is more suitable for intermediate to
+            advanced JavaScript developers who are familiar with concepts from
+            ES6 and up, while Angular favors those same developers who are also
+            familiar with TypeScript.s.
+          </p>
+          <p className="text-lg font-bold">React js Vs Vue</p>
+          <p className="font-bold text-gray-600">
+            The choice between React vs Vue is often debated and it’s not an
+            easy one. Vue has a vibrant and ever-growing community and has taken
+            over popularity vs. React in many respects. React developers are
+            still churning out lots of new components and extras, so there’s no
+            sign that React is on the decline either. Vue is generally more
+            suited to smaller, less complex apps and is easier to learn from
+            scratch compared to React. Vue can be easier to integrate into new
+            or existing projects and many feel its use of HTML templates along
+            with JSX is an advantage. Overall, Vue might be the best choice if
+            you’re a newer developer and not as familiar with advanced
+            JavaScript concepts, while React is quite well suited for
+            experienced programmers and developers who have worked with
+            object-oriented JavaScript, functional JavaScript, and similar
+            concepts.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
